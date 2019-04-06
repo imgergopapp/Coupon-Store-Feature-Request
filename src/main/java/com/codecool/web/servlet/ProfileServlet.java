c4abfc2eaebc6f9217242901c896bb9db977ea26
Gergő Papp <imgergopapp@gmail.com>
2019-04-06 21:18:40 +0200

    Uses getCouponsByUser

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,6 +1,12 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.CouponDao;
+import com.codecool.web.dao.database.DatabaseCouponDao;
+import com.codecool.web.model.Coupon;
 import com.codecool.web.model.User;
+import com.codecool.web.service.CouponService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleCouponService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +14,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/protected/profile")
-public final class ProfileServlet extends HttpServlet {
+public final class ProfileServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = (User) req.getSession().getAttribute("user");
+        List<Coupon> coupons = new ArrayList<>();
+        try (Connection connection = getConnection(req.getServletContext())) {
+
+            CouponDao couponDao = new DatabaseCouponDao(connection);
+            CouponService couponService = new SimpleCouponService(couponDao);
+
+            coupons = couponService.getCouponsByUser(user.getId());
+
+
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        } catch (ServiceException ex) {
+            req.setAttribute("error", ex.getMessage());
+        }
+        req.setAttribute("myCoupons", coupons);
         req.setAttribute("user", user);
         req.getRequestDispatcher("profile.jsp").forward(req, resp);
     }
